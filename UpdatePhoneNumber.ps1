831e04c998ee9baa0cfc66cba783f0db91b2def9
HanSolo71 <HanSolo552@gmail.com>
2019-02-07 10:32:51 -0500

    Add files via upload

---
+++ UpdatePhoneNumber.ps1
@@ -0,0 +1,12 @@
+﻿#CSV file has column Users and Phone
+$PhoneCSVPath = "\\corp.com\fileshare\Phone.csv"
+
+#Import CSV File
+$PhoneCSV = Import-CSV -Path $PhoneCSVPath
+#Apply phone number to each user
+Foreach ($User in $PhoneCSV)
+{
+$Phone = $($User.Phone)
+$Name = $($User.User)
+Set-ADUser -Identity $Name -OfficePhone $Phone
+}
